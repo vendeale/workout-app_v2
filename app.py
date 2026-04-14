622cb079e7c4b3dccd594ff163246bb1ed7ffc32
vendeale <vendeale@gmail.com>
2026-04-14 17:37:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Workout Manager V2", page_icon="🏋️‍♂️"
 st.title("🏋️‍♂️ Registro Allenamenti Cloud")
 
 # Link del tuo foglio (quello che abbiamo visto funzionare)
-URL_FOGLIO = "IL_TUO_LINK_DI_GOOGLE_SHEETS"
+URL_FOGLIO = "https://docs.google.com/spreadsheets/d/1ngWM4rKWmcLDpOH79JDsRQ3QkGj5dkywQ7nTl91x1W4/edit?usp=sharing"
 
 def get_csv_url(url):
     try:
